d803f9395d72aa85ce158cbfa954671fd96547e3
yaswanthkumar55105 <yaswanthkumar.popuri@gmail.com>
2020-05-11 14:53:53 +0530

    First Commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+	has_many :posts
+	has_many :comments
+end
